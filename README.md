216d55b29700de26581aa64ed3c5e03e9747724a
Andrew Chambers <achambers@planittesting.com>
2025-09-30 15:41:26 +1300

    Added schema validation and updated the readme

--- README.md
+++ README.md
@@ -3,8 +3,24 @@
 This example contains an API automation framework using Playwright and Typescript to test the Restful
 Booker API at https://restful-booker.herokuapp.com
 
-It includes both positive and negative tests.
+It includes positive and negative tests and schema validation for valid and invalid schema scenarios.
 
 ## Getting the Latest Code
 
 To get the latest code open your IDE terminal and navigate to where you would like the project to reside (usually a dev folder) and run `git clone https://github.com/druoid/playwright-api-automation.git`
+
+## Setting Up the Environment
+
+To run playwright tests locally, your system must have node installed
+
+1. In a terminal install NVM (Node version manager) with `curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash`
+2. Restart terminal, then install Node.js by running the following command `nvm install node` # latest version
+3. Verify npm is installed by running the following command `npm -v` # Should return the npm version
+4. Install playwright `npm install playwright`
+5. Install browsers `npx playwright install`
+
+## Running the Tests
+
+1. In your IDE's terminal navigate to the root of the project
+2. Run `npm install` to install dependencies
+3. Run `npx playwright test`
---
+++ tests/api/positive/test-case-12-invalid-schema-validation-for-get-booking-by-id.spec.ts
@@ -0,0 +1,46 @@
+import { test, expect } from '@playwright/test';
+import { makePostRequest, makeGetRequest } from '../../../utils/apitUtil';
+import { createBookingData } from '../../../utils/dataFactory';
+import AjvDraft04 from 'ajv-draft-04';
+import * as fs from 'fs';
+import * as path from 'path';
+
+test('Get booking invalid schema validation', async ({ baseURL }) => {
+  // Load the JSON schema from a file
+  const schemaPath = path.resolve('schemas/getBookingJsonSchemaInvalid.json');
+  const schemaData = fs.readFileSync(schemaPath, 'utf-8');
+  const userSchema = JSON.parse(schemaData);    
+
+  // Create booking  
+  const endpoint = '/booking';
+  const createBookingUrl = `${baseURL}${endpoint}`;
+  
+  const postResponse = await makePostRequest(createBookingUrl, createBookingData());
+  expect(postResponse.status()).toEqual(200);
+
+  const postResponseBody = await postResponse.json();
+  const bookingId = postResponseBody.bookingid;
+  
+  const bookingByIdUrl = `${baseURL}${endpoint}/${bookingId}`;
+
+  // Get booking by ID
+  const getResponse = await makeGetRequest(bookingByIdUrl);
+  expect(getResponse.status()).toEqual(200);    
+
+  // Parse the response JSON
+  const responseData = await getResponse.json();
+
+  // Initialize Ajv with Draft 4 support
+  const ajv = new AjvDraft04();
+
+  // Validate the response against the schema
+  const validate = ajv.compile(userSchema);
+  const valid = validate(responseData);
+
+  // Assert that the response matches the schema
+  expect(valid).toBe(false);
+
+  if (!valid) {
+    console.error(validate.errors);
+  }
+});
